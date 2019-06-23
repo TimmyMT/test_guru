fb0f24587d278edfc0fd010dce6416f1db505322
Timmy_MT <mathcore_detected@mail.ru>
2019-06-23 19:15:35 +0400

    23 june 15:15

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,9 @@ class User < ApplicationRecord
   has_many :authors
 
   def show_tests_with_level(level)
-    Test.joins('join tests_users, users on tests.id = tests_users.test_id and users.id = tests_users.user_id')
-        .where(users: {id: self.id}, tests: {level: level})
+    Test.joins('join tests_users
+        on tests.id = tests_users.test_id')
+        .where(tests_users: {user_id: id}, tests: {level: level})
   end
 
 end
