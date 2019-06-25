2e50f4580120621343da7656464e8b3f948ae15e
Timmy_MT <mathcore_detected@mail.ru>
2019-06-25 22:42:59 +0400

    25 june 18:42

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
 # User.destroy_all
 # Question.destroy_all
 # Answer.destroy_all
-
+#
 # User.create!([
 #   {name: 'timmy'},
 #   {name: 'mentor'},
@@ -15,18 +15,18 @@
 #   {title: 'frontend'}
 # ])
 #
-# Category.first.tests.create(title: 'Ruby', creator_id: User.find_by(name: 'mentor').id)
-# Category.last.tests.create(title: 'JavaScript', creator_id: User.find_by(name: 'mentor').id)
-# Category.last.tests.create(title: 'Html', creator_id: User.find_by(name: 'timmy').id)
+# Category.first.tests.create!(title: 'Ruby', creator_id: User.find_by(name: 'mentor').id)
+# Category.last.tests.create!(title: 'JavaScript', creator_id: User.find_by(name: 'mentor').id)
+# Category.last.tests.create!(title: 'Html', creator_id: User.find_by(name: 'timmy').id)
 #
-# Test.find_by(title: 'Ruby').questions.create(body: 'Question 1 for Ruby')
-# Test.find_by(title: 'Ruby').questions.create(body: 'Question 2 for Ruby')
-# Test.find_by(title: 'JavaScript').questions.create(body: 'Question 1 for JS')
-# Test.find_by(title: 'JavaScript').questions.create(body: 'Question 2 for JS')
-# Test.find_by(title: 'Html').questions.create(body: 'Question for html')
+# Test.find_by(title: 'Ruby').questions.create!(body: 'Question 1 for Ruby')
+# Test.find_by(title: 'Ruby').questions.create!(body: 'Question 2 for Ruby')
+# Test.find_by(title: 'JavaScript').questions.create!(body: 'Question 1 for JS')
+# Test.find_by(title: 'JavaScript').questions.create!(body: 'Question 2 for JS')
+# Test.find_by(title: 'Html').questions.create!(body: 'Question for html')
 #
-# Question.find_by(body: 'Question 1 for Ruby').answers.create(body: 'Answer 1 for Ruby')
-# Question.find_by(body: 'Question 2 for Ruby').answers.create(body: 'Answer 2 for Ruby')
-# Question.find_by(body: 'Question 1 for JS').answers.create(body: 'Answer 1 for JS')
-# Question.find_by(body: 'Question 2 for JS').answers.create(body: 'Answer 2 for JS')
-# Question.find_by(body: 'Question for html').answers.create(body: 'Answer for html')
+# Question.find_by(body: 'Question 1 for Ruby').answers.create!(body: 'Answer 1 for Ruby')
+# Question.find_by(body: 'Question 2 for Ruby').answers.create!(body: 'Answer 2 for Ruby')
+# Question.find_by(body: 'Question 1 for JS').answers.create!(body: 'Answer 1 for JS')
+# Question.find_by(body: 'Question 2 for JS').answers.create!(body: 'Answer 2 for JS')
+# Question.find_by(body: 'Question for html').answers.create!(body: 'Answer for html')
