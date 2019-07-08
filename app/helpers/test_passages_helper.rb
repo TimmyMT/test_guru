35e6e35faaf710efbbd983db83349d101921dcf5
Timmy_MT <mathcore_detected@mail.ru>
2019-07-09 00:09:18 +0400

    8 july 20:08, fix color methods, views

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,3 +1,9 @@
 module TestPassagesHelper
-
+  def result_color(test_passage)
+    if test_passage.succesful?
+      'success'
+    else
+      'fail'
+    end
+  end
 end
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -12,19 +12,16 @@ class TestPassage < ApplicationRecord
 
   def current_number
     # test.questions.pluck(:id).find_index(current_question.id) + 1
-    test.questions.count - questions_collection.count
+    # test.questions.count - questions_collection.count
+    test.questions.order(:id).where('id <= ?', current_question.id).count
   end
 
   def result_percent
     ((correct_questions * 100) / test.questions.count.to_f).to_i
   end
 
-  def color_percent
-    if self.result_percent >= 85
-      'success'
-    elsif self.result_percent < 85
-      'fail'
-    end
+  def succesful?
+    true if result_percent >= 85
   end
 
   def completed?
@@ -34,7 +31,8 @@ class TestPassage < ApplicationRecord
   private
 
   def correct_answer?(answer_ids)
-    correct_answers.ids.sort == answer_ids.map(&:to_i).sort if answer_ids.present?
+    # correct_answers.ids.sort == answer_ids.map(&:to_i).sort if answer_ids.present?
+    correct_answers.ids.sort == Array(answer_ids).map(&:to_i).sort
   end
 
   def correct_answers
@@ -46,7 +44,7 @@ class TestPassage < ApplicationRecord
   end
 
   def next_question
-    if completed?
+    if new_record?
       self.current_question = test.questions.first
     else
       self.current_question = questions_collection.first
