f0809178024924572c611b8db8886c7244a859cf
Timmy_MT <mathcore_detected@mail.ru>
2019-07-08 16:22:01 +0400

    8 july 12:21, fix for recoemmended

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,10 +1,3 @@
 module TestPassagesHelper
-  def color_percent(test_passage)
-    if test_passage.result_percent >= 85
-      color = 'green'
-    elsif test_passage.result_percent < 85
-      color = 'red'
-    end
-    "<span style=\"color: #{color}\">#{test_passage.result_percent}%</span>"
-  end
+
 end
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,25 +3,28 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: :Question, optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
-
-  before_update :before_update_change_question
+  before_validation :sef_current_question
 
   def accept!(answer_ids)
-    if correct_answer?(answer_ids)
-      self.correct_questions += 1
-    end
-
-    # self.current_question = next_question
+    self.correct_questions += 1 if correct_answer?(answer_ids)
     save!
   end
 
   def current_number
     test.questions.order(:id).pluck(:id).find_index(current_question.id) + 1
+    # test.questions.order(:id).where('id > ?', current_question.id).index
   end
 
   def result_percent
     ((correct_questions * 100) / test.questions.count.to_f).to_i
+  end
+
+  def color_percent
+    if self.result_percent >= 85
+      'success'
+    elsif self.result_percent < 85
+      'fail'
+    end
   end
 
   def completed?
@@ -30,31 +33,24 @@ class TestPassage < ApplicationRecord
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
-  end
-
   def correct_answer?(answer_ids)
-    # correct_answers_count = correct_answers.count
-    # (correct_answers_count == correct_answers.where(id: answer_ids).count) &&
-    #     correct_answers_count == answer_ids.count
-    correct_answers.ids.sort == answer_ids.map(&:to_i).sort
+    correct_answers.ids.sort == answer_ids.map(&:to_i).sort if answer_ids.present?
   end
 
   def correct_answers
     current_question.answers.correct
   end
 
-  def before_update_change_question
-    if completed?
-      self.current_question = test.questions.order_by(:id).first
-    else
-      self.current_question = next_question
-    end
+  def sef_current_question
+    self.current_question = next_question
   end
 
   def next_question
-    questions_collection.first
+    if completed?
+      self.current_question = test.questions.first
+    else
+      self.current_question = questions_collection.first
+    end
   end
 
   def questions_collection
