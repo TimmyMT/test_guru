fedc769c945f0a9f64673fa74f81b53bf235f807
Timmy_MT <mathcore_detected@mail.ru>
2019-07-11 14:37:22 +0400

    11 july 10:36, delete module auth for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ApplicationRecord
 
-  include Auth
-
   has_many :created_tests, class_name: :Test, foreign_key: :creator_id
   has_many :test_passages
   has_many :tests, through: :test_passages
 
-  # has_secure_password
+  has_secure_password
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
