0954672134df6ae117eceeedf29f84f7d39d2240
Timmy_MT <mathcore_detected@mail.ru>
2019-07-16 19:21:51 +0400

    flash 'welcome user'

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,8 @@
+class Users::SessionsController < Devise::SessionsController
+
+  def create
+    super
+    flash[:notice] = "Hello #{current_user.first_name}!"
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable,
          :registerable,
          :recoverable,
@@ -9,16 +8,12 @@ class User < ApplicationRecord
          :validatable,
          :confirmable
 
-
-  # MAILFORMAT = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-
   has_many :created_tests, class_name: :Test, foreign_key: :creator_id
   has_many :test_passages
   has_many :tests, through: :test_passages
 
-  # validates :email, presence: true, uniqueness: true, format: { with: MAILFORMAT }
-
-  # has_secure_password
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
