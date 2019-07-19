9a659561802131df6a9c9a47e1fedd348c85efea
Timmy_MT <mathcore_detected@mail.ru>
2019-07-19 18:22:24 +0400

    fix flash locale

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,7 @@ class Users::SessionsController < Devise::SessionsController
 
   def create
     super
-    flash[:notice] = "Hello #{current_user.first_name}!"
+    flash[:notice] = t('devise.hello_user', name: current_user.first_name)
   end
 
   private
