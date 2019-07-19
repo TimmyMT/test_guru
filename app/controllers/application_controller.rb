ad8121041736d2b18406a81686e8b1fec032d935
Timmy_MT <mathcore_detected@mail.ru>
2019-07-20 00:28:30 +0400

    second fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def default_url_options
-    if I18n.default_locale == I18n.locale
-      {}
-    else
-      { lang: I18n.locale }
-    end
+    I18n.default_locale == I18n.locale ? {} : { lang: I18n.locale }
   end
 
   private
