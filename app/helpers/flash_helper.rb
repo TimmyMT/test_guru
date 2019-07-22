3811ffaa7170b78a65e245630a6b81c455adf46d
Timmy_MT <mathcore_detected@mail.ru>
2019-07-22 22:23:48 +0400

    add flash helper

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -2,4 +2,13 @@ module FlashHelper
   # def flash_message(type)
   #   content_tag(:div, flash[type], class: "flash #{type}") if flash.present?
   # end
+
+  def flash_convert(type)
+    case type.to_s
+    when 'notice' then 'alert alert-info'
+    when 'alert' then 'alert alert-danger'
+    when 'success' then 'alert alert-success'
+    when 'warning' then 'alert alert-danger'
+    end
+  end
 end
