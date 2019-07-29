668e57845bb6c7f0b08466231f7322b326a55d5e
Timmy_MT <mathcore_detected@mail.ru>
2019-07-29 19:51:28 +0400

    29 july 15:51

--- app/assets/javascripts/utilities/sorting.js
+++ app/assets/javascripts/utilities/sorting.js
@@ -1,7 +1,7 @@
 document.addEventListener('turbolinks:load', function() {
   var control = document.querySelector('.sort-by-title')
 
-  control.addEventListener('click', sortRowsByTitle)
+  if (control) { control.addEventListener('click', sortRowsByTitle) }
 });
 
 function sortRowsByTitle() {
@@ -14,9 +14,16 @@ function sortRowsByTitle() {
     sortedRows.push(rows[i]);
   };
 
-  console.log(sortedRows)
 
-  sortedRows.sort(compareRows);
+  if (this.querySelector('.octicon-arrow-up').classList.contains('hide')) {
+    sortedRows.sort(compareRowsAsc)
+    this.querySelector('.octicon-arrow-up').classList.remove('hide')
+    this.querySelector('.octicon-arrow-down').classList.add('hide')
+  } else {
+    sortedRows.sort(compareRowsDesc)
+    this.querySelector('.octicon-arrow-up').classList.add('hide')
+    this.querySelector('.octicon-arrow-down').classList.remove('hide')
+  }
 
   var sortedTable = document.createElement('table');
 
@@ -30,13 +37,20 @@ function sortRowsByTitle() {
   table.parentNode.replaceChild(sortedTable, table)
 }
 
-function compareRows(row1, row2) {
+function compareRowsAsc(row1, row2) {
   var testTitle1 = row1.querySelector('td').textContent;
-  console.log(testTitle1);
   var testTitle2 = row2.querySelector('td').textContent;
-  console.log(testTitle2);
 
   if (testTitle1 < testTitle2) {return -1}
   if (testTitle1 > testTitle2) {return 1}
+  return 0
+}
+
+function compareRowsDesc(row1, row2) {
+  var testTitle1 = row1.querySelector('td').textContent;
+  var testTitle2 = row2.querySelector('td').textContent;
+
+  if (testTitle1 < testTitle2) {return 1}
+  if (testTitle1 > testTitle2) {return -1}
   return 0
 }
