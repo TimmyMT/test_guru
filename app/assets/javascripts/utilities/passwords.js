c4cdc9c72d2735bc5d1975afd09608cb325f0d3a
Timmy_MT <mathcore_detected@mail.ru>
2019-07-30 14:56:54 +0400

    add JS for password fields

---
+++ app/assets/javascripts/utilities/passwords.js
@@ -0,0 +1,29 @@
+document.addEventListener('turbolinks:load', function() {
+  var passwordConfirm = document.querySelector('#user_password_confirmation')
+
+  if (passwordConfirm) {
+    passwordConfirm.addEventListener('input', writePass)
+  }
+
+  function writePass() {
+    var password = document.querySelector('#user_password')
+    var passwordConfirm = document.querySelector('#user_password_confirmation')
+
+    if (passwordConfirm.value == '') {
+      password.classList.remove('red-field')
+      password.classList.remove('green-field')
+      passwordConfirm.classList.remove('red-field')
+      passwordConfirm.classList.remove('green-field')
+    } else if (password.value == passwordConfirm.value) {
+      password.classList.add('green-field')
+      password.classList.remove('red-field')
+      passwordConfirm.classList.add('green-field')
+      passwordConfirm.classList.remove('red-field')
+    } else {
+      password.classList.add('red-field')
+      password.classList.remove('green-field')
+      passwordConfirm.classList.add('red-field')
+      passwordConfirm.classList.remove('green-field')
+    }
+  }
+});
