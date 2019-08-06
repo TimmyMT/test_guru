8a05154db9a77724974b648c92c71fb560cb75d1
Timmy_MT <mathcore_detected@mail.ru>
2019-08-06 19:00:08 +0400

    change production.rb for action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,15 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: ENV['SMTP_USERNAME'],
+    password: ENV['SMTP_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
