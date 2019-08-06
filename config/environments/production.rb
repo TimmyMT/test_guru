d9b78b608001fab502dc6152cf356ec64b0277c3
Timmy_MT <mathcore_detected@mail.ru>
2019-08-06 19:27:18 +0400

    second fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {host: 'timmy-mt-test-gutu.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp.mail.ru',
+    address: 'smtp.gmail.com',
     port: 587,
     user_name: ENV['SMTP_USERNAME'],
     password: ENV['SMTP_PASSWORD'],
