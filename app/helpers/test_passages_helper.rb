d83d5da26904f8d8228e7ed4b84912e7a8ebe629
Timmy_MT <mathcore_detected@mail.ru>
2019-08-08 19:03:30 +0400

    fix result message

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,6 +1,13 @@
 module TestPassagesHelper
+  def completed_message(test_passage)
+    if test_passage.succesful?
+      t('test_passages.result.header.success', title: test_passage.test.title)
+    else
+      t('test_passages.result.header.fail', title: test_passage.test.title)
+    end
+  end
+
   def result_color(test_passage)
-    test_passage
     if test_passage.succesful?
       'success'
     else
