696b65f59d4d3495f7836fcf8cf8c544a76defc2
Timmy_MT <mathcore_detected@mail.ru>
2019-08-13 18:35:37 +0400

    add 2 methods sort on frist_try/tests_level

--- app/services/badge_control_service.rb
+++ app/services/badge_control_service.rb
@@ -14,11 +14,30 @@ class BadgeControlService
   def all_tests_category(category)
     tests_category_count = Test.where(category_id: Category.find_by(title: category.to_s).id).count
     user_colmpleted_tests_category = @user.test_passages
-        .where(passed: true)
-        .where(test_id: Test.find_by(category_id: Category.find_by(title: category.to_s)))
-        .pluck('DISTINCT test_id').count
-    puts "Tests with Category count = #{tests_category_count}"
-    puts "User tests with Category count = #{user_colmpleted_tests_category}"
+      .where(passed: true)
+      .where(test_id: Test.find_by(category_id: Category.find_by(title: category.to_s)))
+      .pluck('DISTINCT test_id').count
+
+    #puts "Tests with Category count = #{tests_category_count}"
+    #puts "User tests with Category count = #{user_colmpleted_tests_category}"
+    return tests_category_count == user_colmpleted_tests_category
+  end
+
+  def first_try_completed?
+    count_tests = @user.test_passages.where(test_id: @test_passage.test.id).count
+    return count_tests == 1 && @test_passage.passed == true
+  end
+
+  def all_tests_level(level)
+    count_tests = Test.all.where(level: level.to_i).count
+    user_count_tests = @user.test_passages
+      .where(passed: true)
+      .where(test_id: Test.find_by(level: level.to_i))
+      .pluck('DISTINCT test_id')
+      .count
+    # puts "count tests with level #{count_tests}"
+    # puts "count user tests with level #{user_count_tests}"
+    return count_tests == user_count_tests && count_tests != 0
   end
 
 end
