0d3db2579a2e6cdd1581979e3e7942083631e4ea
Timmy_MT <mathcore_detected@mail.ru>
2019-08-15 18:20:27 +0400

    renamed variables for BadgesCS

--- app/services/badge_control_service.rb
+++ app/services/badge_control_service.rb
@@ -13,31 +13,31 @@ class BadgeControlService
   private
 
   def control_1(category)
-    category_tests = Test.joins('INNER JOIN categories ON categories.id = tests.category_id')
+    all_tests_for_category = Test.joins('INNER JOIN categories ON categories.id = tests.category_id')
                                 .where(categories: {title: category})
                                 .order(id: :desc).pluck(:id)
 
-    user_completed_tests = @user.test_passages.joins('INNER JOIN tests ON tests.id = test_passages.test_id
+    user_tests_for_category = @user.test_passages.joins('INNER JOIN tests ON tests.id = test_passages.test_id
                                                       INNER JOIN categories ON categories.id = tests.category_id')
                                                     .where(test_passages: {passed: true}, categories: {title: category})
                                                     .order(id: :desc).pluck(:test_id)
 
-    category_tests == user_completed_tests
+    all_tests_for_category == user_tests_for_category
   end
 
   def control_2(nothing)
-    count_tests = @user.test_passages.where(test_id: @test_passage.test.id).where(passed: true).count
-    count_tests == 1 && nothing == ''
+    test_first_try = @user.test_passages.where(test_id: @test_passage.test.id).where(passed: true).count
+    test_first_try == 1 && nothing == ''
   end
 
   def control_3(level)
-    level_tests = Test.where(level: level.to_i).order(id: :desc).pluck(:id)
+    all_tests_for_level = Test.where(level: level.to_i).order(id: :desc).pluck(:id)
 
-    user_tests = @user.test_passages.joins('join tests on tests.id = test_passages.test_id')
+    user_tests_for_level = @user.test_passages.joins('join tests on tests.id = test_passages.test_id')
       .where(tests: {level: level.to_i}, test_passages: {passed: true})
       .order(test_id: :desc).pluck(:test_id)
 
-    level_tests == user_tests
+    all_tests_for_level == user_tests_for_level
   end
 
 end
