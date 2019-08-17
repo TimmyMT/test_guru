a220153e85c3153694764fb4ba972e956dc1f84a
Timmy_MT <mathcore_detected@mail.ru>
2019-08-17 17:12:25 +0400

    17 aug 13:12

--- app/services/badge_control_service.rb
+++ app/services/badge_control_service.rb
@@ -6,38 +6,45 @@ class BadgeControlService
   end
 
   def call
-    badges = []
-    badges << Badge.all.select{ |badge| send("control_#{badge.control}", badge.control_param) unless @user.badges.include?(badge) && badge.control_param != '' }
+    Badge.select{|badge| send("control_#{badge.control}", badge)}
+  end
+
+  def user_tests
+    @user.test_passages.where(passed: true)
+  end
+
+  def badge_present?(badge)
+    @user.badges.include?(badge)
   end
 
   private
 
-  def control_1(category)
+  def control_1(badge)
     all_tests_for_category = Test.joins('INNER JOIN categories ON categories.id = tests.category_id')
-                                .where(categories: {title: category})
+                                .where(categories: {title: badge.control_param.to_s})
                                 .order(id: :desc).pluck(:id)
 
-    user_tests_for_category = @user.test_passages.joins('INNER JOIN tests ON tests.id = test_passages.test_id
-                                                      INNER JOIN categories ON categories.id = tests.category_id')
-                                                    .where(test_passages: {passed: true}, categories: {title: category})
-                                                    .order(id: :desc).pluck(:test_id)
+    user_tests_for_category = user_tests.joins('INNER JOIN tests ON tests.id = test_passages.test_id
+                                INNER JOIN categories ON categories.id = tests.category_id')
+                                .where(categories: {title: badge.control_param.to_s})
+                                .order(id: :desc).pluck(:test_id)
 
-    all_tests_for_category == user_tests_for_category
+    all_tests_for_category == user_tests_for_category && badge_present?(badge) == false
   end
 
-  def control_2(nothing)
-    test_first_try = @user.test_passages.where(test_id: @test_passage.test.id).where(passed: true).count
-    test_first_try == 1 && nothing == ''
+  def control_2(badge)
+    test_first_try = user_tests.where(test_id: @test_passage.test.id).count
+    test_first_try == 1 && badge.control_param == ''
   end
 
-  def control_3(level)
-    all_tests_for_level = Test.where(level: level.to_i).order(id: :desc).pluck(:id)
+  def control_3(badge)
+    all_tests_for_level = Test.where(level: badge.control_param.to_i).order(id: :desc).pluck(:id)
 
-    user_tests_for_level = @user.test_passages.joins('join tests on tests.id = test_passages.test_id')
-      .where(tests: {level: level.to_i}, test_passages: {passed: true})
-      .order(test_id: :desc).pluck(:test_id)
+    user_tests_for_level = user_tests.joins('join tests on tests.id = test_passages.test_id')
+                             .where(tests: {level: badge.control_param.to_i})
+                             .order(test_id: :desc).pluck(:test_id)
 
-    all_tests_for_level == user_tests_for_level
+    all_tests_for_level == user_tests_for_level && badge_present?(badge) == false
   end
 
 end
