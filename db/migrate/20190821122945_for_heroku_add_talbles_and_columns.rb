3a3dd7e3316847cce789478fa04f077e027b692a
Timmy_MT <mathcore_detected@mail.ru>
2019-08-22 15:55:58 +0400

    22 aug 11:55

--- db/migrate/20190821122945_for_heroku_add_talbles_and_columns.rb
+++ db/migrate/20190821122945_for_heroku_add_talbles_and_columns.rb
@@ -1,25 +1,25 @@
 class ForHerokuAddTalblesAndColumns < ActiveRecord::Migration[5.2]
   def change
-    create_table "badge_users", force: :cascade do |t|
-      t.bigint "user_id"
-      t.bigint "badge_id"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-      t.index ["badge_id"], name: "index_badge_users_on_badge_id"
-      t.index ["user_id"], name: "index_badge_users_on_user_id"
-    end
-
-    create_table "badges", force: :cascade do |t|
-      t.string "name"
-      t.text "picture"
-      t.integer "control"
-      t.string "control_param"
-      t.datetime "created_at", null: false
-      t.datetime "updated_at", null: false
-    end
-
-    add_column :test_passages, :passed, :boolean
-    add_column :test_passages, :time_over, :boolean, default: false
-    add_column :tests, :timeleft, :integer
+    # create_table "badge_users", force: :cascade do |t|
+    #   t.bigint "user_id"
+    #   t.bigint "badge_id"
+    #   t.datetime "created_at", null: false
+    #   t.datetime "updated_at", null: false
+    #   t.index ["badge_id"], name: "index_badge_users_on_badge_id"
+    #   t.index ["user_id"], name: "index_badge_users_on_user_id"
+    # end
+    #
+    # create_table "badges", force: :cascade do |t|
+    #   t.string "name"
+    #   t.text "picture"
+    #   t.integer "control"
+    #   t.string "control_param"
+    #   t.datetime "created_at", null: false
+    #   t.datetime "updated_at", null: false
+    # end
+    #
+    # add_column :test_passages, :passed, :boolean
+    # add_column :test_passages, :time_over, :boolean, default: false
+    # add_column :tests, :timeleft, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_161621) do
+ActiveRecord::Schema.define(version: 2019_08_21_122945) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
